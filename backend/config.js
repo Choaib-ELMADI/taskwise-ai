4dd4db96b2082a5ab894f7ea9ce49397b270bb37
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-09-14 22:39:56 +0100

    Connect Mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const MONGODB_URL =
+	"mongodb+srv://choaibelmadi:edDYyx20du6rtoIb@cluster0.1ujojeq.mongodb.net/taskwizeai?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
+import mongoose from "mongoose";
 import express from "express";
 
-import { PORT } from "./config.js";
+import { PORT, MONGODB_URL } from "./config.js";
 
 const app = express();
-app.listen(PORT, () => {
-	console.log(`App listening at port ${PORT}`);
+
+app.get("/", (request, response) => {
+	return response.status(200).send("Hello, From Server");
 });
+
+mongoose
+	.connect(MONGODB_URL)
+	.then(() => {
+		console.log("Connected to db");
+		app.listen(PORT, () => {
+			console.log(`App listening at port ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
