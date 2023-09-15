0f8f38ca1af2fe3bd46c5c41509de3f1246eaf7d
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-09-15 08:51:54 +0100

    Set up tailwindcss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
