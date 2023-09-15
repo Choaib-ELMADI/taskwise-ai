0e91d4ad6ea3196d056bb86380a3a0ebed196931
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-09-15 08:03:12 +0100

    Move all tasks routes into its file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import express from "express";
 
+import tasksRoutes from "./routes/tasksRoutes.js";
 import { PORT, MONGODB_URL } from "./config.js";
-import { Task } from "./models/taskModel.js";
 
 const app = express();
 app.use(express.json());
@@ -11,87 +11,7 @@ app.get("/", (request, response) => {
 	return response.status(200).send("Hello, From Server");
 });
 
-app.post("/tasks", async (request, response) => {
-	try {
-		const {
-			body: { title, description, priority, user },
-		} = request;
-
-		if (!title || !description || !priority || !user) {
-			return response.status(400).send({ message: "Missing required data" });
-		}
-
-		const newTask = { title, description, priority, user };
-		const task = await Task.create(newTask);
-		return response.status(201).send(task);
-	} catch (err) {
-		console.log(err.message);
-		return response.status(500).send({ message: err.message });
-	}
-});
-
-app.get("/tasks", async (request, response) => {
-	try {
-		const tasks = await Task.find({});
-		return response.status(200).json({
-			count: tasks.length,
-			data: tasks,
-		});
-	} catch (err) {
-		console.log(err.message);
-		return response.status(500).send({ message: err.message });
-	}
-});
-
-app.get("/tasks/:id", async (request, response) => {
-	try {
-		const { id } = request.params;
-
-		const task = await Task.findById(id);
-		return response.status(200).json(task);
-	} catch (err) {
-		console.log(err.message);
-		return response.status(500).send({ message: err.message });
-	}
-});
-
-app.put("/tasks/:id", async (request, response) => {
-	try {
-		const {
-			body: { title, description, priority, user },
-			params: { id },
-		} = request;
-
-		if (!title || !description || !priority || !user) {
-			return response.status(400).send({ message: "Missing required data" });
-		}
-
-		const updatedTask = await Task.findByIdAndUpdate(id, request.body);
-		if (!updatedTask) {
-			return response.status(404).json({ message: "Task to update not found" });
-		}
-
-		return response.status(200).send({ message: "Task updated successfully" });
-	} catch (err) {
-		console.log(err.message);
-		return response.status(500).send({ message: err.message });
-	}
-});
-
-app.delete("/tasks/:id", async (request, response) => {
-	try {
-		const { id } = request.params;
-
-		const taskToDelete = await Task.findByIdAndDelete(id);
-		if (!taskToDelete) {
-			return response.status(404).json({ message: "Task to delete not found" });
-		}
-		return response.status(200).json({ message: "Task deleted successfully" });
-	} catch (err) {
-		console.log(err.message);
-		return response.status(500).send({ message: err.message });
-	}
-});
+app.use("/tasks", tasksRoutes);
 
 mongoose
 	.connect(MONGODB_URL)
