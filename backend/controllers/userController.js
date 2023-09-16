0bd43604810f953e404815c875b59f032c519870
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-09-16 12:33:42 +0100

    Sign up users with token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 import { User } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+	return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "4d" });
+};
 
 export const loginUser = async (request, response) => {
 	return response.status(200).json({ message: "Login user" });
@@ -9,9 +14,11 @@ export const signupUser = async (request, response) => {
 		const { email, password } = request.body;
 
 		const user = await User.signup(email, password);
+		const token = createToken(user._id);
+
 		return response
 			.status(200)
-			.json({ message: "User created successfully", email, user });
+			.json({ message: "User created successfully", token });
 	} catch (err) {
 		console.log(err.message);
 		return response
