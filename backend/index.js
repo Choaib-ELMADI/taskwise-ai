07b87929ecfee92ea14ccdecc16800a5dfb49e9d
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-09-20 21:30:58 +0100

    Update url

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: [""],
+		origin: ["https://task-wise-ai-frontend.vercel.app/"],
 		methods: ["POST", "GET", "DELETE", "PUT"],
 		credentials: true,
 	})
